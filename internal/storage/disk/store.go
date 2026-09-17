7166c6aa30e185de480c135655c3091a9c0f4a7e
Oleg Burenkov <burenkov.oleg@gmail.com>
2026-09-18 00:59:05 +0300

    fix: remove legacy secret fields from profile cloning

--- internal/storage/disk/store.go
+++ internal/storage/disk/store.go
@@ -711,8 +711,6 @@ func cloneProfile(profile sessions.Profile) sessions.Profile {
 			cloned.Options[key] = value
 		}
 	}
-	cloned.Password = sessions.EncryptedString(strings.TrimSpace(string(profile.Password)))
-	cloned.KeyPassphrase = sessions.EncryptedString(strings.TrimSpace(string(profile.KeyPassphrase)))
 	return cloned
 }
 
