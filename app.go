10df96e32a3ba837a7566190325305ce15b0d0ae
Oleg Burenkov <burenkov.oleg@gmail.com>
2026-09-19 01:56:51 +0300

    Expose reconnect session API

--- app.go
+++ app.go
@@ -55,6 +55,7 @@ func (a *App) GetShellState() app.ShellState { return a.currentService().GetShel
 func (a *App) GetReleaseVersion() string { return resolveReleaseVersion() }
 func (a *App) LaunchSession(profileID string) (app.RuntimeSessionView, error) { return a.currentService().LaunchSession(profileID) }
 func (a *App) CloseSession(sessionID string) error { return a.currentService().CloseSession(sessionID) }
+func (a *App) ReconnectSession(sessionID string) error { return a.currentService().ReconnectSession(sessionID) }
 func (a *App) CreateSessionProfile(input sessions.ProfileInput) error { return a.currentService().CreateSessionProfileInput(input) }
 func (a *App) DeleteSessionProfile(id string) error { return a.currentService().DeleteSessionProfile(id) }
 func (a *App) ConnectSSH(tabID, profileID string) error { return a.currentService().ConnectSSH(a.ctx, tabID, profileID) }
