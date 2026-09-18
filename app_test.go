3b653cf816e3177de27f74a1d689694f7398a8eb
Oleg Burenkov <burenkov.oleg@gmail.com>
2026-09-18 23:59:06 +0300

    refactor: remove RDP support

--- app_test.go
+++ app_test.go
@@ -1,12 +1,1 @@
 package main
-
-import (
-	"testing"
-)
-
-func TestOpenRDPFailsForUnknownProfile(t *testing.T) {
-	app := NewApp()
-	if err := app.OpenRDP("rdp-tab", "unknown-profile"); err == nil {
-		t.Fatal("expected error for unknown RDP profile")
-	}
-}
