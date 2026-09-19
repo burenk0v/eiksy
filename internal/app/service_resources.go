5a666a3cb891cba014506543a130aa2fe9b92b46
Oleg Burenkov <burenkov.oleg@gmail.com>
2026-09-19 14:30:42 +0300

    fix: remove unused workspace import

--- internal/app/service_resources.go
+++ internal/app/service_resources.go
@@ -6,7 +6,6 @@ import (
 
 	"eiksy/internal/domain/protocols"
 	"eiksy/internal/domain/resources"
-	"eiksy/internal/domain/workspace"
 )
 
 // GetResources returns configured operational targets without exposing
